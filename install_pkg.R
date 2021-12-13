868724f369b2c6a7af1f722cc0db4e7d5d8c7375
GusHebblewhite <ajhebblewhite@gmail.com>
2021-12-13 21:37:42 +1100

    initial commit

---
+++ install_pkg.R
@@ -0,0 +1,5 @@
+install_pkg = function(package){
+    install.packages(package, 
+                     "C:/Users/Molly/Anaconda3/envs/R/R/library", 
+                     repos='http://cran.us.r-project.org')
+}
